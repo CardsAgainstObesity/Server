f18c580540def66bafc24616299100690c567ee1
Zlendy <argutierrezg11@gmail.com>
2022-04-01 23:46:43 +0200

    Separación de backend y frontend en dos repositorios

--- frontend/src/components/icons/IconPlayerCzar.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <svg style="width: 24px; vertical-align: bottom" viewBox="0 0 24 24">
-    <path
-      fill="currentColor"
-      d="M5 16L3 5L8.5 10L12 4L15.5 10L21 5L19 16H5M19 19C19 19.6 18.6 20 18 20H6C5.4 20 5 19.6 5 19V18H19V19Z"
-    />
-  </svg>
-</template>
