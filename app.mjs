b2531ad0a1e93b891e1b733a7b0e7c44902c251a
Zlendy <argutierrezg11@gmail.com>
2022-04-10 23:21:38 +0200

    Port completo a HTTP/2 (WS usa HTTP/1.1 como fallback, todo lo demas no)

--- app.mjs
+++ app.mjs
@@ -1,39 +1,41 @@
-import express from 'express';
 import { createRequire } from "module";
-import http from 'http';
-import https from 'https';
-import path from 'path';
+import http2 from 'http2';
 import { readFileSync } from 'fs';
 import LoggingSystem from './src/util/LoggingSystem.mjs';
 import GameServer from './src/service/GameServer.mjs';
 import 'dotenv/config';
+import serveStatic from 'serve-static';
+import finalhandler from 'finalhandler';
 
 const require = createRequire(import.meta.url);
 const config = require("./config.json");
-const secure_server = false;
 
-const app = express();
-app.use(express.static(process.env.FRONTEND_PATH+'/dist'));
+let serve = serveStatic(process.env.FRONTEND_PATH + '/dist', { index: 'index.html', fallthrough: false })
 
-app.get('*', function (req, res) { // Redirect all URLs to VueJS
-    res.sendFile(process.env.FRONTEND_PATH+'/dist/index.html');
-});
-    
-// HTTP Server instance
-const options = {
+// HTTP Server configuration
+const options = { // TODO: Set values in config.json
     key: readFileSync('src/openssl/key.pem'),
     cert: readFileSync('src/openssl/cert.pem'),
     passphrase: 'abcd',
+    allowHTTP1: true
 }
-let server;
-if (secure_server) server = https.createServer(options, app);
-else server = http.createServer(app);
+
+// Create server
+let server = http2.createSecureServer(options, onRequest);
+
+function onRequest(req, res) { // HTTP/1.1 Syntax
+    let done = finalhandler(req, res);
+    serve(req, res, (err) => {
+        if (err) return res.end(readFileSync(process.env.FRONTEND_PATH + '/dist/index.html')); // Redirect non matching URLs to index.html
+        return done();
+    });
+}
 
 // Start listening for requests on configured port
-server.listen(secure_server ? config.secure_port:config.port, () => {
+server.listen(config.secure_port, () => {
     let host = server.address().address + ":" + server.address().port;
     LoggingSystem.singleton.log("[WEB]", "Listening to " + host);
 });
 
-// Link GameServer to HTTP server
+// Link GameServer to HTTP/2 server
 GameServer.singleton.listen(server);
